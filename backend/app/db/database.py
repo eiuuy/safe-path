65f54be0d4b70632ae03b442685418ca47536d8e
eiuuy <iqanat.yessen@gmail.com>
2026-04-22 17:02:23 +0500

    Update database.py

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-
+from dotenv import load_dotenv
 load_dotenv()
 
 # Твой URL теперь БЕЗ "?sslmode=require"
